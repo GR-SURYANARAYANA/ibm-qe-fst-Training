5790489423cfe52974af446132de2d5f8a78f881
SuryanarayanaGR <suryagr14@gmail.com>
2026-01-29 11:44:27 +0530

    xpath usage

--- src/main/java/org/seliniumTraining/ActivityThree.java
+++ src/main/java/org/seliniumTraining/ActivityThree.java
@@ -1,4 +1,27 @@
 package org.seliniumTraining;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class ActivityThree {
-}
+    public static void main(String[] args) {
+            WebDriver driver = new FirefoxDriver();
+            driver.get("https://training-support.net/webelements/login-form/");
+            System.out.println("The title of Current Page is :"+ driver.getTitle());
+            WebElement userName = driver.findElement(By.xpath("//input[@name = 'username']"));
+
+            WebElement passwordElement = driver.findElement(By.xpath("//input[@name = 'password']"));
+            System.out.println(userName.getAttribute("placeholder"));
+            System.out.println(passwordElement.getAttribute("placeholder"));
+            userName.sendKeys("admin");
+            passwordElement.sendKeys("password");
+
+            WebElement submitButton = driver.findElement(By.xpath("//button[text()='Submit']"));
+            submitButton.click();
+            System.out.println("Click is done sucessfull");
+            System.out.println("The new title of the page is : "+ driver.getTitle());
+            System.out.println("The new url located is : "+ driver.getCurrentUrl());
+        }
+    }

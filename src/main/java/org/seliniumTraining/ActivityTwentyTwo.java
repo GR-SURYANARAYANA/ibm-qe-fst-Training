6da39f11331df0f9b02c6cce4f8433d08002e555
SuryanarayanaGR <suryagr14@gmail.com>
2026-02-02 12:23:41 +0530

    pop up

--- src/main/java/org/seliniumTraining/ActivityTwentyTwo.java
+++ src/main/java/org/seliniumTraining/ActivityTwentyTwo.java
@@ -1,4 +1,23 @@
 package org.seliniumTraining;
 
 public class ActivityTwentyTwo {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/popups");
+        WebElement button = driver.findElement(By.id("launcher"));
+        button.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("username")));
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        username.sendKeys("admin");
+        password.sendKeys("password");
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+        String message = driver.findElement(By.cssSelector("h2.text-center")).getText();
+        System.out.println("Login message: " + message);
+
+
+
+
+    }
 }

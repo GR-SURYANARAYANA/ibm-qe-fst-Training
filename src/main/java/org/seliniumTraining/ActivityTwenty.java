9dfe6803bf1cf973fabf7c97fa1f6ffe3a9060bb
SuryanarayanaGR <suryagr14@gmail.com>
2026-02-02 11:24:33 +0530

    Alert Activity

--- src/main/java/org/seliniumTraining/ActivityTwenty.java
+++ src/main/java/org/seliniumTraining/ActivityTwenty.java
@@ -1,4 +1,23 @@
 package org.seliniumTraining;
 
-public class ActivityTWenty {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class ActivityTwenty {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/alerts");
+        WebElement promptButton = driver.findElement(By.id("prompt"));
+        promptButton.click();
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.sendKeys("Hi suryanarayana..!! you are in Awesome.! Class ");
+        alert.accept();
+        driver.close();
+    }
 }
